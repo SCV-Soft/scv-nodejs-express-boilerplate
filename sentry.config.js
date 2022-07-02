b25d84a7f568ee17efd8b4667201c6a613f431d3
benjioh5 <benjioh5@gmail.com>
2022-07-03 03:08:14 +0900

    add sentry

---
+++ sentry.config.js
@@ -0,0 +1,9 @@
+import * as Sentry from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.SENTRY_DSN;
+
+if (process.env.VERCEL_ENV === "production")
+	Sentry.init({
+		dsn: SENTRY_DSN,
+		tracesSampleRate: 1.0,
+	});
